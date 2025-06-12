9a4203ec1434d602a2f0717d3193ed8ad106b101
golfeado <leonardosoteldo@gmail.com>
2025-06-11 21:29:26 -0400

    Solution to problem 2.3 (donuts in 2. Designing)

--- 2-designing/donuts/schema.sql
+++ 2-designing/donuts/schema.sql
@@ -1,5 +1,57 @@
--- See `eww' bookmarks or go to:
--- https://cs50.harvard.edu/sql/2024/psets/2/donuts/usq_donuts.webp
+PRAGMA foreign_keys = ON;
 
--- Use `PRAGMA foreign_keys` to see if FOREIGN KEYS are enabled. Enabled them
--- with ` PRAGMA foreign_keys = ON`
+CREATE TABLE ingredients (
+       id             INTEGER PRIMARY KEY,
+       ingredient     TEXT    NOT NULL,
+       unit           TEXT    NOT NULL,
+       price_per_unit DECIMAL NOT NULL
+                              CHECK(price_per_unit >= 0)
+);
+
+CREATE TABLE donuts (
+       id          INTEGER PRIMARY KEY,
+       name        TEXT    NOT NULL,
+       gluten_free INTEGER DEFAULT 0
+                           CHECK(gluten_free IN (0, 1)),
+       price       DECIMAL NOT NULL
+                           CHECK(price >= 0)
+);
+
+CREATE TABLE ingredients_in_donuts (
+       id            INTEGER PRIMARY KEY,
+       donut_id      INTEGER,
+       ingredient_id INTEGER,
+       FOREIGN KEY(donut_id) REFERENCES donuts(id),
+       FOREIGN KEY(ingredient_id) REFERENCES ingredients(id)
+);
+
+CREATE TABLE orders (
+       id           INTEGER PRIMARY KEY,
+       customer_id  INTEGER,
+       order_num    INTEGER NOT NULL,
+       FOREIGN KEY(customer_id) REFERENCES customers(id)
+);
+
+CREATE TABLE donuts_in_orders (
+       id        INTEGER PRIMARY KEY,
+       order_id  INTEGER,
+       donut_id  INTEGER,
+       FOREIGN KEY(order_id) REFERENCES orders(id),
+       FOREIGN KEY(donut_id) REFERENCES donuts(id)
+);
+
+CREATE TABLE customers (
+       id         INTEGER PRIMARY KEY,
+       first_name TEXT,
+       last_name  TEXT
+);
+
+CREATE TABLE customer_orders (
+       id          INTEGER PRIMARY KEY,
+       customer_id INTEGER,
+       order_id    INTEGER,
+       FOREIGN KEY(customer_id) REFERENCES customers(id),
+       FOREIGN KEY(order_id) REFERENCES orders(id)       
+);
+
+
